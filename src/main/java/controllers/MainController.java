2bf0dfb83a1dc887e5dd2b7d2ac1b61ce1a37e63
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-20 18:47:39 +0100

    deploy project to heroku

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -15,4 +15,12 @@ public class MainController {
         Seeds.seedData();
     }
 
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
 }
